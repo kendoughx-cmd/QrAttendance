dd7a5ef86219aea79479584d147cd1587662681a
Kent <yagongkent@gmail.com>
2024-01-14 21:15:36 +0800

    modify

--- routes/TabNavigator.js
+++ routes/TabNavigator.js
@@ -1,6 +1,7 @@
 // TabNavigator.js
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import LogScreen from "../screens/LogScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -9,7 +10,28 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Log") {
+            iconName = "list-alt";
+          } else if (route.name === "Profile") {
+            iconName = "user";
+          }
+
+          // You can return any component here that you want as the tab bar icon
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "blue",
+        inactiveTintColor: "gray",
+      }}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Log" component={LogScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
